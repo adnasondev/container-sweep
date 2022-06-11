c0a2a64c4cb37fb3ef46f78495450de86d2f083a
adnason <adnason@ncsu.edu>
2022-06-11 12:34:31 -0500

    made progress with kill command

--- commands/kill.js
+++ commands/kill.js
@@ -1,9 +1,25 @@
+const Job = require('cron').CronJob
+const Docker = require('dockerode');
+
+const dockerClient = new Docker({
+    host: '192.168.65.10',
+    port: process.env.DOCKER_PORT || 2375,
+    socketPath: '/var/run/docker.sock'
+});
+
 exports.command = 'kill';
-exports.desc = 'Deletes a Docable Notebook';
+exports.desc = 'Deletes a Docable Notebook.';
 exports.builder = yargs => {
     yargs.options({
+        'name': {
+            alias: 'n',
+            describe: 'The name of containers to delete.',
+            demandOption: true,
+            type: 'string'
+        },
     });
 };
 
 exports.handler = async argv => {
+    console.log(dockerClient)
 };
