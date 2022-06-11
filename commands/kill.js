a65bce047a9cdaea3052db008f7b643865f321af
adnason <adnason@ncsu.edu>
2022-06-11 18:44:01 -0500

    Feature: Implemented part of kill command

--- commands/kill.js
+++ commands/kill.js
@@ -1,25 +1,66 @@
-const Job = require('cron').CronJob
 const Docker = require('dockerode');
+const docker = new Docker()
 
-const dockerClient = new Docker({
-    host: '192.168.65.10',
-    port: process.env.DOCKER_PORT || 2375,
-    socketPath: '/var/run/docker.sock'
-});
+const MINUTES_IN_HOUR = 60
+
+function nameMeetsCriteria(containerName, criteriaName) {
+    if (criteriaName === '*') {
+        return true
+    } else {
+        return criteriaName.includes(containerName)
+    }
+}
+
+function ageMeetsCriteria(containerAge, criteriaAge) {
+    return containerAge >= criteriaAge
+}
+
+function containerMeetsCriteria(container, criteria) {
+    containerName = container.Names[0]
+    containerAge = parseInt(container.Status.replace(/^\D+/g, '')) * MINUTES_IN_HOUR
+    criteriaName = criteria.name
+    criteriaAge = parseInt(criteria.age)
+
+    if (nameMeetsCriteria(containerName, criteriaName) && ageMeetsCriteria(containerAge, criteriaAge)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+async function killContainer(container) {
+    await docker.getContainer(container.Id).remove({ force: true });
+}
+
+async function killContainersHandler(criteria) {
+    docker.listContainers((error, containers) => {
+        if (containers) {
+            containers.forEach(async container => {
+                if (containerMeetsCriteria(container, criteria)) {
+                    await killContainer(container)
+                }
+            })
+        }
+    })
+}
 
 exports.command = 'kill';
 exports.desc = 'Deletes a Docable Notebook.';
 exports.builder = yargs => {
     yargs.options({
         'name': {
-            alias: 'n',
             describe: 'The name of containers to delete.',
-            demandOption: true,
+            default: '*',
             type: 'string'
         },
+        'age': {
+            describe: 'The age of containers to delete.',
+            default: '0',
+            type: 'string'
+        }
     });
 };
 
-exports.handler = async argv => {
-    console.log(dockerClient)
+exports.handler = async arguments => {
+    killContainersHandler(arguments)
 };
---
+++ test/createContainers.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for i in {1..5}
+do
+    CONTAINER_NAME=$(openssl rand -base64 12)
+    echo "Creating container: $CONTAINER_NAME"
+    docker run --name $CONTAINER_NAME -d -t alpine /bin/sh
+done
